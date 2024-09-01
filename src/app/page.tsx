aad1d7d9e3eb59b97f9d6123ca17047919c9bc78
abelchac <abelchac@seas.upenn.edu>
2024-09-01 13:57:28 -0400

    #5 added a fade in animation for cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+"use client";
 import Image from "next/image";
+
+import React, { useRef, useCallback, use, useEffect } from "react";
+import { useInView, InView} from "react-intersection-observer";
 
 
 function ProjectCard(props : {project_svg_name : string, alt_text : string}){
   let data_string = './cards/' + props.project_svg_name;
-  console.log(data_string);
-  return (<img className="place-items-center" src={data_string} alt={props.alt_text}/>
+  
+  const { ref, inView, entry } = useInView(
+    {
+      /* Optional config */
+      threshold: 0.3,  // Trigger when 10% of the element is in view
+      triggerOnce: true,  // Trigger only once
+    }
+  );
+
+  if (inView) {
+    entry?.target.classList.add('show');
+  } else{
+    entry?.target.classList.remove('show');
+  }
+  console.log(inView);
+  
+
+  return (
+    <InView as="div" onChange={(inView, entry) => {entry.target.classList.add('show')}}>
+      <img ref={ref} className={`place-items-center ProjectCard transition-opacity duration-1000 ${inView ? 'opacity-100' : 'opacity-0'}`} src={data_string} alt={props.alt_text}/>
+    </InView>
   );
 }
 
@@ -12,11 +35,12 @@ function ProjectCard(props : {project_svg_name : string, alt_text : string}){
 export default function Home() {
 
   //let card_csgo = create_card("CSGO_CARD.svg");
+
   return (
     
   <span>
   <div>
-    <div className="grid grid-cols-[repeat(auto-fill,20rem)] gap-x-16 gap-y-10 border-2 p-8 justify-center">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(10rem,25rem))] gap-x-16 gap-y-10 border-2 p-8 justify-center">
       <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
       <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
       <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
