8bb21a162991765c5dfe8af1b93321431f0b2e00
abelchac <abelchac@seas.upenn.edu>
2024-08-31 22:00:04 -0400

    #4 made grid for cards to sit in that flows with screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
 
-function ProjectCard(props : {project_svg_name : string}){
+function ProjectCard(props : {project_svg_name : string, alt_text : string}){
   let data_string = './cards/' + props.project_svg_name;
   console.log(data_string);
-  return (<img src="./cards/CSGO_CARD.png"/>);
+  return (<img className="place-items-center" src={data_string} alt={props.alt_text}/>
+  );
 }
 
 
@@ -14,12 +15,26 @@ export default function Home() {
   return (
     
   <span>
+  <div>
+    <div className="grid grid-cols-[repeat(auto-fill,20rem)] gap-x-16 gap-y-10 border-2 p-8 justify-center">
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+      <ProjectCard project_svg_name="CSGO_CARD.png" alt_text=""/>
+    </div>
+  </div>
+    <ProjectCard project_svg_name="CSGO_CARD.png" alt_text="Cards that looks like a trading card that has the title: 'CSGO WIN Predictor', 
+    attributes: Big Data, Machine Learning, Data Visualization. 
+    Description: Using feed forward neural networks and CSGO player statistics, a game result was predicted "/>
 
-    <ProjectCard project_svg_name="CSGO_CARD.svg"/>
-    <ProjectCard project_svg_name="CSGO_CARD.svg"/>
-    <ProjectCard project_svg_name="CSGO_CARD.svg"/>
-    <ProjectCard project_svg_name="CSGO_CARD.svg"/>
-    <ProjectCard project_svg_name="CSGO_CARD.svg"/>
     
  
   </span> 
