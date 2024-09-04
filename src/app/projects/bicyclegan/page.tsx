ab0ee80b7133370594dca95162e3658ca1b9e6fa
abelchac <abelchac@seas.upenn.edu>
2024-09-04 12:11:43 -0400

    making bicyclegan page and finished nba awards #15 #16

--- src/app/projects/bicyclegan/page.tsx
+++ src/app/projects/bicyclegan/page.tsx
@@ -25,10 +25,151 @@ function fadeInFactory(element : React.JSX.Element){
 
 
 export default function BicycleGANProject() {
+  let title = (<div className=""><h1>BicycleGAN</h1>
+    <Link href="https://github.com/abelchac/bicyclegan"> <h2>Project Link: https://github.com/abelchac/bicyclegan</h2></Link>
+    </div>);
 
+  let abstract = fadeInFactory(<div>
+    <h2>Abstract</h2>
+    <p>In the problem of single image-to-image translation, the model learns a single output for each
+input image of the edge given, with ambiguous generation. Thus in the many image-to-image
+models we must create a method to create multiple outputs on a distribution. We use a generator
+that takes in a latent vector that is randomly sampled to develop this distribution. We used both
+“add to all” and “and to input” when considering where we include our latent vector.
+</p>
+    <img src="bicyclegan/"></img>
+  </div>)
+
+let problem = fadeInFactory(<div>
+  <h2>1 Problem/Introduction</h2>
+  <p>The problem that is proposed is to develop a method to generate a many image-to-image model,
+meaning that we can create many different realistic outputs based on a single image. Our
+approach to the problem is to implement BicycleGAN which is an extension of CycleGAN that
+links basic GAN and VAE models in order to map an edge image into multiple RGB images,
+which CycleGAN is not capable of.
+</p>
+  <img src="bicyclegan/"></img>
+</div>)
+
+let lit_review = fadeInFactory(<div>
+  <h2>2 Literature Review
+  </h2>
+  <p>BicycleGAN consists of two major modules, cVAE-GAN and cLR-GAN.
+cVAE-GAN encodes a ground truth image B and then attempts to map an image A into B [1].
+The Conditional Variational Autoencoder GAN is an approach that generates the latent vector
+from the ground truth image. The model should be able to create a specific image from the input.
+This method gives the model a “peek” into desired output. The Conditional Latent Regressor
+GAN takes a randomly generated latent vector and the image to generate an image that is
+realistic, but not necessarily the ground truth image. The point of the BicycleGAN is to combine
+these 2 methods to create a connection between the latent vector and the encoding to create a
+diverse and conditional set of output images [1].
+</p>
+  <img src="bicyclegan/"></img>
+</div>)
+
+let exp_sum = fadeInFactory(<div>
+  <h2>3 Experiments Summary</h2>
+  <p>We began our experiments using the Edges2shoes dataset. This data set consists of paired images
+of a single object and its edges, for a total resolution of 512 by 256 pixels. Each object takes up
+the entire image. We will first split the paired image into the ground truth RGB image and edge
+image, and reduce both to a resolution of 128 by 128 pixels. We will also normalize the pixel
+values of the object image to the range [-1 to 1]. The images have white backgrounds. This
+should provide good performance for the model, as there is little to no noise and good resolution.
+For experimentation we used both the “add to all” and “and to input” methods for the inclusion
+of the latent vector. Our findings showed that using the “add to all” method, much like in the
+article, gave more noise that allowed for a more normal distribution of possible output images.</p>
+  <img src="bicyclegan/"></img>
+</div>)
+
+let method = fadeInFactory(<div>
+  <h2>4 Methodology</h2>
+  <p>For our final generator model, we implemented a similar UNet generator used in Linder-Norén’s
+Pytorch Bicycle GAN implementation [2].</p>
+  <p>Generator (UNetDown Modules):</p>
+  <p>The input of each UNetDown module (denoted by color) is the output of the previous module, or
+the image in the case of the first module, concatenated with the latent code after being processed
+by a linear layer:
+</p>
+  <p>This adds the latent code to every layer of the UNet generator.</p>
+  <p>Generator (Up Modules):</p>
+  <p>The input of the first UNetUp module (denoted by color) is the output of UNetDown modules 5
+and 6 concatenated. The input of every subsequent UNetUp module at layer i is the output of the
+previous UNetUp module, and its output is concatenated with the input to the UNetDown
+module at layer 6 - i.
+</p>
+  <p>Finally, we take the output of the UNetUp modules and perform final processing, shown below:</p>
+  <p>For our discriminator model we use PatchGAN, shown below:</p>
+  <p>The output shape of our discriminator is (1, 5x5).</p>
+  <p>For training, we use two separate discriminators and a single generator. For each training image,
+We first generate a random latent code, encode it, then generate a prediction from each
+discriminator. We calculate loss for the encoder by taking the sum of GAN loss, using BCE
+criterion, for each discriminator prediction, L1 pixel loss, and L1 KL divergence loss. We
+calculate loss for the generator by taking the L1 loss for latent code reconstruction. We calculate
+loss for the discriminators by summing BCE for the valid label with prediction for a true image
+and the fake label with prediction for a fake image.</p>
+  <img src="bicyclegan/"></img>
+</div>)
+
+let results_sum = fadeInFactory(<div>
+  <h2>5 Summary Of Results</h2>
+  <h3>Evaluation Metrics</h3>
+  <h4>Visualization of Training:</h4>
+  <img src="bicyclegan/"></img>
+  <img src="bicyclegan/"></img>
+  <img src="bicyclegan/"></img>
+  <img src="bicyclegan/"></img>
+  <h4>Step 0:</h4>
+  <img src="bicyclegan/"></img>
+  <h4>Step 500:</h4>
+  <img src="bicyclegan/"></img>
+  <h4>Step 1000:</h4>
+  <img src="bicyclegan/"></img>
+  <h4>Step 4000:</h4>
+  <img src="bicyclegan/"></img>
+  <h4>Step 99000:</h4>
+  <img src="bicyclegan/"></img>
+  <h3>Quantitative Evaluations</h3>
+  <h3>Qualitative Evaluations</h3>
+  <img src="bicyclegan/"></img>
+</div>)
+
+let conclusion = fadeInFactory(<div>
+  <h2>Conclusion
+  </h2>
+  <p>The results that are created by our model are diverse and are conditionally based on our latent
+vector. This was aided by the use of the “add to all” method which adds the latent vector to all
+layers of the generator instead of just the input. The realistic nature of the images that are
+produced could deceive at first glance, but when considering the FID score of over 200, we can
+see that these images are not seen as realistic. A possible change that could have been made was
+to use a learnable upscale such as a transposed convolution rather than just a raw upscale that has
+no learnable features. However, the results produced do give confidence in the validity of the
+BicycleGAN model and the use of a latent vector to create bijective mappings of multi-modal
+image outputs.
+</p>
+</div>)
+
+let references = fadeInFactory(<div>
+  <h2>References</h2>
+  <p>[1] Zhu, J. Y., Zhang, R., Pathak, D., Darrell, T., Efros, A. A., Wang, O., & Shechtman, E. (2017).
+Multimodal Image-to-Image Translation by Enforcing Bi-Cycle Consistency. In Advances in neural
+information processing systems (pp. 465-476).</p>
+<p>[2] https://github.com/eriklindernoren/PyTorch-GAN/tree/master/implementations/bicyclegan</p>
+</div>)
+ 
 
   return (
       <section className="">
+      {title}
+      <section>
+      {abstract}
+      </section>
+      {problem}
+      {lit_review}
+      {exp_sum}
+      {method}
+      {results_sum}
+      {conclusion}
+      {references}
       
       </section>     
      
--- src/app/projects/nba_awards_predictor/page.tsx
+++ src/app/projects/nba_awards_predictor/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import React, { useRef, useCallback, use, useEffect } from "react";
 import { useInView, InView} from "react-intersection-observer";
 import { CodeBlock, dracula } from 'react-code-blocks';
+import { start } from "repl";
 
 
 
@@ -25,11 +26,200 @@ function fadeInFactory(element : React.JSX.Element){
 
 
 export default function NbaAwardsProject() {
+  let title = (<div className=""><h1>Nba Awards Predictor</h1>
+    <Link href="https://github.com/abelchac/nba_awards_predictor"> <h2>Project Link: https://github.com/abelchac/nba_awards_predictor</h2></Link>
+    </div>);
+
+  let description = fadeInFactory(<div>
+    <h2>Description:</h2>
+    <p>Description: We are interested in the NBA and particularly how different awards are given out. As a result,
+designed a program that takes into the account the statistics of each player in the league and calculates
+a score using the data. We begin with a table that displays each player and their statistics. From there, 
+depending on the award chosen (MVP, ROTY, DPOY), uses different statistics to narrow down candidates for
+each award and show which players have the best statistics for the award. Furthermore, the weight that 
+each statistic carries in the calculations can be modified to best fit the user's preference for which 
+statistics that matter most. We also have a MVP-BFS function that finds a path of players, beginning with
+an MVP and through their teammates, creating a path to another MVP. All in all, we have designed a
+program that compiles the statistics of all current NBA players in a table. From there, we can narrow down
+the table based on whichever award is chosen by completing calculation with their statistics to obtain an
+award score, which is automatically sorted to list the best players fit for that award. Finally, we have 
+also included an additional function that finds the shortest path between one MVP to another MVP using 
+teammates. </p>
+  </div>);
+
+  let relevant_cat = fadeInFactory(<div>
+    <h2>Relevant categories:</h2>
+    <p>Our projects uses Document Search or Information Retrieval techniques to find and 
+store each player's statistics in a table. This is seen in several places. To compile the main table, we
+retrieve information from <Link href="https://www.nba.com/stats/"> https://www.nba.com/stats/</Link> and store them as JSON Objects. The second place
+we use this is in the MVP-BFS function, where we obtain teammates for each MVP and their teammates as
+well, using the base link: <Link href="https://basketball.realgm.com/">https://basketball.realgm.com/</Link>, and modifying it to obtain a new link and 
+getting additional information there.</p>
+<br></br>
+<p>Also in the MVP-BFS section, we use Graph and Graph Algorithm concepts. We first a build a graph from the
+MVPs, their teammates, and teammates of those teammates. We then use BFS to find a path from one MVP and 
+following their teammates and teammates of those teammates to another MVP.
+</p>
+  </div>);
+    
+  let startScreen = fadeInFactory(<div>
+
+    <h2>Start the Program:</h2>
+    <h2>Initial Screen:</h2>
+    <p>The base table shows all current players in the NBA with all traditional statistics</p>
+    <img src="nba_awards_predictor/start.png"></img>
+  </div>);
+
+  let buttons = fadeInFactory(<div>
+
+    <h2>Buttons:</h2>
+    <img src="nba_awards_predictor/buttons.png"></img>
+    <p>The buttons to the right of the table allow the user to select which actions to take.</p>
+    <p> MVP (Most Valuble Player): Will open up the MVP predictor screen</p>
+    <p>>DPOY (Defensive Player Of The Year): Will open up the DPOY predictor screen</p>
+    <p>ROTY (Rookie Of The Year): Will open up the ROTY predictor screen</p>
+    <p>MVP-BFS: Will open up the MVP-BFS screen</p>
+    <p>RESET: Reset the tables and take the user back to the initial screen</p>
+  </div>);    
+  
+  let mvp = fadeInFactory(<div>
+
+    <h2>MVP </h2>
+    <p>The MVP predictor screen will appear like this with initial weights on the sliders set. These sliders can be
+changed and the weight changes will be reflected on the sliders and the MVP score function. These values
+will compute new MVP Scores which are placed into the MVP score column for each player. This column
+is auto sorted so that the player with the highest score is at the top.</p>
+<img src="nba_awards_predictor/mvp.png"></img>
+  </div>);
+  
+let mvpSliders = fadeInFactory(<div>
+
+  <h2>MVP SLIDERS</h2>
+  <p>To select the weights for the MVP score function, use the sliders that are to the right of
+the table if you wish to adjust how player statistics should be reflected in an MVP
+candidate.</p>
+  <img src="nba_awards_predictor/mvp_sliders.png"></img>
+
+</div>);
+  
+let mvp_score = fadeInFactory(<div>
+
+  <h2>MVP SCORE FUNCTION
+  </h2>
+  <p>The MVP SCORE function takes the weights from the sliders and changes the label of
+  present in the frame, so that the user can see how all the weights apply.</p>
+  <img src="nba_awards_predictor/mvp_score.png"></img>
+
+</div>);
+
+let dpoy = fadeInFactory(<div>
+
+  <h2>DPOY</h2>
+  <p>The DPOY predictor screen will appear like this with initial weights on the sliders set. These sliders can
+be changed and the weight changes will be reflected on the sliders and the DPOY score function. These
+values will compute new DPOY Scores which are placed into the DPOY score column for each player.
+This column is auto sorted so that the player with the highest score is at the top.</p>
+  <img src="nba_awards_predictor/dpoy.png"></img>
+
+</div>);
+
+let dpoy_sliders = fadeInFactory(<div>
+
+  <h2>DPOY SLIDERS
+  </h2>
+  <p>To select the weights for the DPOY score function, use the sliders that are to the right of
+the table if you wish to adjust how player statistics should be reflected in an DPOY
+candidate</p>
+  <img src="nba_awards_predictor/dpoy_sliders.png"></img>
+
+</div>);
+
+let dpoy_score = fadeInFactory(<div>
+
+  <h2>DPOY SCORE FUNCTION</h2>
+  <p>The DPOY SCORE function takes the weights from the sliders and changes the label of
+  present in the frame, so that the user can see how all the weights apply.</p>
+  <img src="nba_awards_predictor/dpoy_score.png"></img>
+
+</div>);
+
+let roty = fadeInFactory(<div>
+
+  <h2>ROTY</h2>
+  <p>The ROTY predictor screen will appear like this with initial weights on the sliders set. These sliders can
+be changed and the weight changes will be reflected on the sliders and the ROTY score function. These
+values will compute new ROTY Scores which are placed into the ROTY score column for each player.
+This column is auto sorted so that the player with the highest score is at the top.
+</p>
+  <img src="nba_awards_predictor/roy.png"></img>
+
+</div>);
+
+let roty_sliders = fadeInFactory(<div>
+
+  <h2>ROTY SLIDERS
+  </h2>
+  <p>To select the weights for the ROTY score function, use the sliders that are to the right of
+the table if you wish to adjust how player statistics should be reflected in an ROTY
+candidate.</p>
+  <img src="nba_awards_predictor/roy_sliders.png"></img>
+
+</div>);
+
+let roty_score = fadeInFactory(<div>
+
+  <h2>ROTY SCORE FUNCTION</h2>
+  <p>The ROTY SCORE function takes the weights from the sliders and changes the label of
+  present in the frame, so that the user can see how all the weights apply</p>
+  <img src="nba_awards_predictor/roy_score.png"></img>
+
+</div>);
+
+let mvp_bfs = fadeInFactory(<div>
+
+  <h2>MVP-BFS</h2>
+  <p>Select a former MVP winner from each of the 2 lists and press find path to generate the shortest
+path of teammates that will go from one MVP to the other. Without the playerGraph.ser file, the
+MVP-BFS will not be able to run until the graph is fully scrapped from
+<Link href="https://basketball.realgm.com/">https://basketball.realgm.com/</Link>. Therefore, we have included the playerGraph.ser file so that the
+user will not have to wait for the graph to be built to be able to use the MVP-BFS functionality.
+However, all other aspects of the program will be full functionally because of the MVP-BFS
+graph being built on another thread</p>
+  <img src="nba_awards_predictor/mvp_bfs.png"></img>
+
+</div>);
 
 
   return (
       <section className="">
-      
+        {title}
+        <section>
+        {description}
+        <br></br>
+        {relevant_cat}
+        </section>
+        <br></br>
+        {startScreen}
+        <br></br>
+        {mvp}
+        <section>
+          {mvpSliders}
+          {mvp_score}
+        </section>
+        <br></br>
+        {dpoy}
+        <section>
+          {dpoy_sliders}
+          {dpoy_score}
+        </section>
+        <br></br>
+        {roty}
+        <section>
+          {roty_sliders}
+          {roty_score}
+        </section>
+        <br></br>
+        {mvp_bfs}
       </section>     
      
     );
